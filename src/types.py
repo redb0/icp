ef3731643ff1ab7c8f9e7206f3beab880a337a15
Vladimir <voronov.volodya2013@yandex.ru>
2021-11-14 21:45:52 +0700

    add Rectangle protocol

--- src/types.py
+++ src/types.py
@@ -5,11 +5,32 @@
     - Voronov Vladimir
 """
 
-from typing import TypeAlias
+from typing import Protocol, TypeAlias
 
 
 Number: TypeAlias = int | float
 Coord: TypeAlias = tuple[Number, Number]
 # Size: TypeAlias = tuple[Number, Number] | list[Number]
 Size: TypeAlias = tuple[Number, Number]
+Point: TypeAlias = tuple[Number, Number]
 SizeList: TypeAlias = list[Size]
+
+
+class RectangleProtocol(Protocol):
+    """Протокол прямоугольника"""
+    length: Number
+    width: Number
+    is_rotatable: bool
+
+    # @property
+    # def size(self) -> Size:
+    #     raise NotImplementedError
+
+    def rotate(self) -> None:
+        """Поворот на 90 градусов"""
+        raise NotImplementedError
+
+    @property
+    def area(self) -> Number:
+        """Площадь фигуры"""
+        raise NotImplementedError
